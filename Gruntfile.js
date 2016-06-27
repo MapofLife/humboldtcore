f4f6afae73353117a0b0ebaea9f6b9b65529a362
Jeremy Malczyk <jmalczyk@gmail.com>
2016-06-27 08:46:04 -0400

    Updating build

--- Gruntfile.js
+++ Gruntfile.js
@@ -40,7 +40,7 @@ module.exports = function(grunt) {
     copy : {
       main: {
         files: [
-          { expand:true, nonull: false, cwd: 'src', src: ['static/app/**/*.{html,png,jpg,xslx}'], dest: 'dist/'}
+          { expand:true, nonull: false, cwd: 'src', src: ['static/app/**/*.{html,png,jpg,xlsx}'], dest: 'dist/'}
         ],
       },
     },
